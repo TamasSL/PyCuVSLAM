7f0fc968b19b582aee2fbae0d946139105d0f1c9
TamasSL <tamas@sectorlabs.ro>
2025-11-21 10:56:26 +0200

    save frames conditionally

--- examples/realsense/server.py
+++ examples/realsense/server.py
@@ -81,6 +81,7 @@ class SensorStreamServicer(sensor_stream_pb2_grpc.SensorStreamServiceServicer):
         self.initial_plan = True
         self.long_term_goal_planner = ExplorationPlanner(grid_resolution=0.1, safety_distance=0.3)
         self.frame_id = 0
+        self.save_frames_to_disk = False
 
         # Create some parameters
         projective_integrator_params = ProjectiveIntegratorParams()
@@ -284,8 +285,6 @@ class SensorStreamServicer(sensor_stream_pb2_grpc.SensorStreamServiceServicer):
         if dataload_timer is not None:
             dataload_timer.stop()
 
-        self.frame_id += 1
-
         # Do reconstruction using the depth
         with Timer('depth'):
             T_W_C_left_infrared = to_homogeneous(position, orientation)
@@ -295,7 +294,7 @@ class SensorStreamServicer(sensor_stream_pb2_grpc.SensorStreamServiceServicer):
                 depth = torch.from_numpy(depth_image).float().to('cuda')     
                 depth_intrinsics = torch.from_numpy(rs_intrinsics_to_matrix(self.depth_intrinsic)).float()      
                 self.nvblox_mapper.add_depth_frame(depth, T_W_C_left_infrared, depth_intrinsics)
-                self.save_depth(frame_id, depth)
+                self.save_depth(depth)
 
         with Timer('color'):
             if T_W_C_left_infrared is not None and \
@@ -305,7 +304,7 @@ class SensorStreamServicer(sensor_stream_pb2_grpc.SensorStreamServiceServicer):
                 color = torch.from_numpy(color_image).to('cuda')     
                 color_intrinsics = torch.from_numpy(rs_intrinsics_to_matrix(self.color_intrinsic)).float()      
                 self.nvblox_mapper.add_color_frame(color, T_W_C_color, color_intrinsics)
-                self.save_image(frame_id, color_image)
+                self.save_image(color_image)
 
         with Timer('visualize_rerun'):
             # Visualize pose. This occurs every time we track.
@@ -397,15 +396,23 @@ class SensorStreamServicer(sensor_stream_pb2_grpc.SensorStreamServiceServicer):
         print("written")
         print(success)
 
-
-        def save_image(self, frame_id: int, image: np.ndarray, quality: int = 80):
-            """Compress image to JPEG"""
+    def save_image(self, image: np.ndarray, quality: int = 80):
+        """Compress image to JPEG"""
+        if self.save_frames_to_disk:
+            self.frame_id += 1
             encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
-            cv2.imwrite(f'images/rgb_{frame_id}.jpg', image, encode_param)
-    
-        def save_depth(self, frame_id: int, depth: np.ndarray):
-            """Compress depth using PNG (lossless)"""
-            cv2.imwrite(f'images/depth_{frame_id}.png', depth)
+            cv2.imwrite(f'images/rgb_{self.frame_id}.jpg', image, encode_param)
+
+    def save_depth(self, depth: np.ndarray):
+        """Compress depth using PNG (lossless)"""
+        if self.save_frames_to_disk
+            cv2.imwrite(f'images/depth_{self.frame_id}.png', depth)
+
+    def enable_saving_frames(self):
+        self.save_frames_to_disk = True
+
+    def enable_saving_frames(self):
+        self.save_frames_to_disk = False
 
 async def manual_control(servicer):
         """Example: Send commands from server keyboard"""
@@ -440,6 +447,12 @@ async def manual_control(servicer):
             
             elif cmd == 'r':
                 servicer.export_recording()
+
+            elif cmd == 's':
+                servicer.enable_saving_frames()
+
+            elif cmd == 'd':
+                servicer.disable_saving_frames()
 
             elif cmd == 'q':
                 break
